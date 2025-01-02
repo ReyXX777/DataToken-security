5176d25b281f1b67ca065bd7c8cfb02bf5e78e0d
ReyXX777 <cse2021100@rcciit.org.in>
2025-01-02 23:48:47 +0530

    Update autoload.php
    PSR-4 Overloading

---
+++ vendor/autoload.php
@@ -0,0 +1,13 @@
+{
+    "name": "yourname/data-token-security",
+    "description": "A project that implements tokenization and encryption for sensitive data.",
+    "require": {
+        "php": "^7.3",
+        "ext-openssl": "*"
+    },
+    "autoload": {
+        "psr-4": {
+            "YourNamespace\\": "src/"
+        }
+    }
+}
