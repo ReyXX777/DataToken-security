db95b47d8a2f4800d5bcdf5788ccb7c5135231d6
AzadXx77 <cse2021100@rcciit.org.in>
2025-01-16 23:57:04 +0530

    Update autoload.php
    // Commit message: Added environment variable management and testing/linting scripts for better development workflow

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -3,11 +3,16 @@
     "description": "A project that implements tokenization and encryption for sensitive data.",
     "require": {
         "php": "^7.3",
-        "ext-openssl": "*"
+        "ext-openssl": "*",
+        "vlucas/phpdotenv": "^5.3"  // New dependency for environment variable management
     },
     "autoload": {
         "psr-4": {
             "YourNamespace\\": "src/"
         }
+    },
+    "scripts": {
+        "test": "phpunit",  // New component: Add a test script for running PHPUnit
+        "lint": "phpcs --standard=PSR12 src/"  // New component: Add a linting script for code style checks
     }
 }
